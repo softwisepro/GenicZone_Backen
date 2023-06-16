815279e95f12b6e6f037c11c66cd65b64acdeea9
E r i c  M w i j a g e <122126027+softwisepro@users.noreply.github.com>
2023-06-16 01:42:20 +0000

    commit

--- stuck/settings.py
+++ stuck/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
+    'https://geniczone.vercel.app',
 )
 
 REST_FRAMEWORK = {
