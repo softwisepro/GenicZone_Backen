db13a7b6ebf35cd6abbbca76e1871e096324a8ba
E r i c  M w i j a g e <122126027+softwisepro@users.noreply.github.com>
2023-06-17 10:18:39 +0000

    commit

--- Pin/models.py
+++ Pin/models.py
@@ -4,12 +4,13 @@ from django.utils.text import slugify
 from django.contrib.auth.models import User
 from user.models import UserProfile
 from autoslug import AutoSlugField
+from cloudinary.models import CloudinaryField
 
 class Pin(models.Model):
     title = models.CharField(max_length=settings.POST_TITLE_MAX_LENGTH)
     slug = AutoSlugField(populate_from='title', unique_with='published_date')
     content = models.TextField(null=True, blank=True)
-    image = models.FileField(default='post_uploads/placeholder.png', upload_to='post_uploads')
+    image = CloudinaryField(default='post_uploads/placeholder.png', upload_to='post_uploads')
     published_date = models.DateTimeField(auto_now_add=False, auto_now=True, null=False, blank=False)
     likes = models.ManyToManyField(User, related_name='likes')
     updated = models.DateTimeField(auto_now=True)
--- user/models.py
+++ user/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, default='')
     last_name = models.CharField(max_length=255, default='')
     username = models.CharField(max_length=255, default='')
-    profile_image = models.FileField(default='profile.png', blank=True, null=True, upload_to='profile_uploads')
-    cover_photo = models.FileField(default='cover.png', blank=True, null=True, upload_to='cover_photo_uploads')
+    profile_image = CloudinaryField(default='profile.png', blank=True, null=True, upload_to='profile_uploads')
+    cover_photo = CloudinaryField(default='cover.png', blank=True, null=True, upload_to='cover_photo_uploads')
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
