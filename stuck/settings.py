8f53d0effbe8935dd018ed6ce275a2d69a9575a8
E r i c  M w i j a g e <122126027+softwisepro@users.noreply.github.com>
2023-06-17 11:38:16 +0000

    commit

--- stuck/settings.py
+++ stuck/settings.py
@@ -133,13 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 
 # Default primary key field type
@@ -159,11 +159,11 @@ REST_FRAMEWORK = {
     )
 }
 
-cloudinary.config({ 
-  cloud_name: 'dhanwmvpb', 
-  api_key: '435879159673217', 
-  api_secret: 'PNEyNzjhyfUFmy8azpwMY_s_lZM' 
-});
+cloudinary.config( 
+  cloud_name = 'dhanwmvpb', 
+  api_key = '435879159673217', 
+  api_secret = 'PNEyNzjhyfUFmy8azpwMY_s_lZM' 
+)
 
 POST_TITLE_MAX_LENGTH = 255
 POST_UNIQUE_SLUG_MAX_LENGTH = 50
--- stuck/urls.py
+++ stuck/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('user.urls'))
 
 ]
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
